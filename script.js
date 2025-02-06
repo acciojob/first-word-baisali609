5d0b665b4412ef53cdc611e1d5fdacef33dc2c89
baisali609 <baisali609@gmail.com>
2025-02-06 12:03:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 function firstWord(s) {
   // your code here
- let str = s.trim(); // Remove leading and trailing spaces
-    let words = str.split(" "); // Split by space
-
-    return words[0];
-	
+	s = s.trim();
+    let spaceIndex = s.indexOf(" ");
+    if (spaceIndex === -1) {
+        return s;
+    }
+    return s.substring(0, spaceIndex);
 }
-console.log(firstWord("see and stop"));  
-console.log(firstWord(" Hello World!")); 
-console.log(firstWord("12345"));         
-console.log(firstWord(""));              
-
-
-Do not change the code below
+// Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
