de11d584659bdb4fcd7cbe36eefcb7413e719190
baisali609 <baisali609@gmail.com>
2025-02-06 11:47:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstWord(s) {
   // your code here
+ str = str.trim(); 
+	let index = str.indexOf(" "); 
+    
+    return index === -1 ? str : str.substring(0, index);
+	
 }
 
-// Do not change the code below
+Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
